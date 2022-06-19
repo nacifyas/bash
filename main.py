770271fe14e239ad8a4e7a01a63d0b5faa8e7f1f
Yassine Nacif <nacifyas@gmail.com>
2022-06-19 14:18:03 +0200

    Improved the script

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ CURRENT_IP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 DOMAIN_IP = socket.gethostbyname(DOMAIN)
 
 def send_email():
-    cmd = '''echo Atomflare Public IP address has changed. The new IP is: {}  | mail -s "IP ADDRESS CHANGE NOTIFICATION" atomflare.events@gmail.com'''.format(CURRENT_IP)
+    cmd = '''echo Atomflare Public IP address has changed. Cloudflare registries states it is {}. The current IP address is: {}  | mail -s "IP ADDRESS CHANGE NOTIFICATION" atomflare.events@gmail.com'''.format(DOMAIN_IP, CURRENT_IP)
     os.system(cmd)
 
 def main():
